9a689dd8cc91adb52286b36b875d696e6e2ac24e
Edwin Ropain E. <edwinropain260@outlook.com>
2025-01-24 18:31:29 -0500

    Cambio de vista

--- index.js
+++ index.js
@@ -6,15 +6,22 @@ const addressUser = document.getElementById("addressUser");
 const phoneNumberUser = document.getElementById("phoneNumberUser");
 const emailUser = document.getElementById("emailUser");
 const btnSaveUser = document.getElementById("btnSaveUser");
+let saveUserModal = document.getElementById("titleModal");
+let updateUserModal = document.getElementById("titleModal");
 
-
-showAllUsers();
-
+function updateUser(id){
+  updateUser.innerText = "dgergegfg";
+  //console.log(id);
+  let user = usersList.filter(user => user.idDocument === id);
+  console.log(user[0]);
+}
 
 /*** API consume ***/
 
 //show all users
 showAllUsers();
+
+let usersList = []
 
 async function showAllUsers(){
   tableUsers.innerHTML =''
@@ -22,9 +29,9 @@ async function showAllUsers(){
   .then(response => response.json())
   .then(users => {
     console.log(users)
-
+    usersList = users;
     for(let user of users){
-      tableUsers.innerHTML += 
+      tableUsers.innerHTML +=
       '<tr>'+
         '<td>'+user.id+'</td>'+
         '<td>'+user.name+'</td>'+
@@ -34,8 +41,8 @@ async function showAllUsers(){
         '<td>'+user.phoneNumber+'</td>'+
         '<td>'+user.email+'</td>'+
         '<td>'+
-          '<button type="button" class="btn btn-warning">Editar</button>'+
-          '<button type="button" class="btn btn-danger">Borrar</button>'+
+          '<button class="btn btn-warning" id="editUserModal" onclick="updateUser('+user.id+')">Editar</button>'+
+          '<button class="btn btn-danger">Borrar</button>'+
         '</td>'+
       '</tr>'
     }
@@ -78,12 +85,19 @@ async function saveUser(){
   })
 }
 
-/** Modal save user ***/
-const OpenModal = document.querySelector("#btnOpenModal");
+/** Modal***/
+const newUserModal = document.querySelector("#newUserModal");
+const editUserModal = document.querySelector("#editUserModal");
 const closeModal = document.querySelector("#btnCloseModal");
 const modal = document.querySelector("#modal");
 
-OpenModal.addEventListener("click",()=>{
+newUserModal.addEventListener("click",()=>{
+  saveUserModal.innerText = "Nuevo usuario"
+  modal.showModal();
+});
+
+editUserModal.addEventListener("click",()=>{
+  updateUserModal.innerText = "Nuevo usuario"
   modal.showModal();
 });
 
