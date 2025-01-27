5dea44898617ac7550b5efa192324630dcd35e12
Edwin Ropain E. <edwinropain260@outlook.com>
2025-01-26 22:16:09 -0500

    Commit cierre

--- index.js
+++ index.js
@@ -1,21 +1,120 @@
 //Variables Globales
-const tablaProductosHTML = document.getElementById("tablaProductos");
-const nameUser = document.getElementById("nameUser");
-const lastName = document.getElementById("lastName");
-const idDocument = document.getElementById("idDocument");
-const address = document.getElementById("address");
-const phoneNumber = document.getElementById("phoneNumber");
+const tablaProductosHTML = document.getElementById('tablaProductos')
+const nameUser =  document.getElementById('nameUser')
+const lastName =  document.getElementById('lastName')
+const idDocument =  document.getElementById('idDocument')
+const address =  document.getElementById('address')
+const phoneNumber =  document.getElementById('phoneNumber')
 const emailUser = document.getElementById("emailUser");
-const btnGuardar = document.getElementById("btnGuardar");
-let modalTitle = document.getElementById("modalTitle");
-let idProductoModificar = 0;
-let listaProductos = []
+const btnGuardar =  document.getElementById('btnGuardar')
+let modalTitle = document.getElementById('modalTitle')
+let idProductoModificar = 0
+
+
+
+//Acciones Guardar Producto
+btnGuardar.addEventListener('click',()=>{
+  if(modalTitle.innerText === "Modificar producto"){
+    crearProducto(idProductoModificar)
+      
+  }
+  else if(modalTitle.innerText === "Crear producto"){
+      crearProducto(0)
+  }
+})
+
+
+//Eliminar Producto
+async function eliminarProducto(id) {
+  await fetch("http://localhost:8080/api/users/delete/" + id, {
+    method: "DELETE",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => {
+      console.log(res);
+      alert("Producto eliminado");
+      obtenerProductos();
+    })
+    .catch((err) => {
+      console.log(err);
+      alert("Lo sentimos no pudimos eliminar el producto");
+    });
+}
+
+
+
+//Update user
+function modificarProducto(id) {
+  modalTitle.innerText = "Modificar producto";
+  modal.style.display = "block";
+  idProductoModificar = id;
+  resetModal();
+  let producto = listaProductos.filter(producto => producto.id === id)
+  console.log(producto[0]);
+  nameUser.value = producto[0].name
+  lastName.value = producto[0].lastName
+  idDocument.value = producto[0].idDocument
+  address.value = producto[0].address
+  phoneNumber.value = producto[0].phoneNumber
+  emailUser.value = producto[0].email
+}
+
+
+
+
+
+/*
+btnGuardar.addEventListener("click", () => {
+  if (modalTitle.innerText === "Modificar Producto") {
+    crearProducto(idProductoModificar);
+  }
+  else if (modalTitle.innerText === "Nuevo usuario") {
+    crearProducto(0);
+  }
+});
+*/
+
+async function crearProducto(id) {
+  let productoGuardar = {
+    id:id,
+    name: nameUser.value,
+    lastName: lastName.value,
+    idDocument: idDocument.value,
+    address: address.value,
+    phoneNumber: phoneNumber.value,
+    email:emailUser.value
+  };
+  console.log(productoGuardar);
+
+  await fetch("http://localhost:8080/api/users/save", {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(productoGuardar),
+  })
+    .then((producto) => {
+      console.log(producto);
+      alert("Usuario registrado");
+      modal.style.display = "none";
+      obtenerProductos();
+      resetModal();
+    })
+    .catch((e) => {
+      console.log(e);
+      alert("Error: " + e);
+    });
+}
 
 
 
 //show all users
 obtenerProductos()
-
+let listaProductos = []
 async function obtenerProductos(){
     tablaProductosHTML.innerHTML = ''
     await fetch('http://localhost:8080/api/users/findAll')
@@ -47,97 +146,13 @@ async function obtenerProductos(){
         })
 }
 
-//Save user
-/*
-btnGuardar.addEventListener("click", () => {
-  if (modalTitle.innerText === "Modificar Producto") {
-    crearProducto(idProductoModificar);
-  } else if (modalTitle.innerText === "Nuevo usuario") {
-    crearProducto();
-  }
-});
-*/
-btnGuardar.addEventListener("click", () => {
-  if (modalTitle.innerText === "Modificar Producto") {
-    crearProducto(idProductoModificar);
-  }
-  else if (modalTitle.innerText === "Nuevo usuario") {
-    crearProducto(0);
-  }
-});
 
 
-async function crearProducto(id) {
-  let productoGuardar = {
-    id: id,
-    name: nameUser.value,
-    lastName: lastName.value,
-    idDocument: idDocument.value,
-    address: address.value,
-    phoneNumber: phoneNumber.value,
-    email:emailUser.value
-  };
-  console.log(productoGuardar);
-
-  await fetch("http://localhost:8080/api/users/save", {
-    method: "POST",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(productoGuardar),
-  })
-    .then((producto) => {
-      console.log(producto);
-      alert("Usuario registrado");
-      modal.style.display = "none";
-      obtenerProductos();
-      resetModal();
-    })
-    .catch((e) => {
-      console.log(e);
-      alert("Error: " + e);
-    });
-}
-
-//Update user
-function modificarProducto(id) {
-  modalTitle.innerText = "Modificar Producto";
-  modal.style.display = "block";
-  idProductoModificar = id;
-  resetModal();
-  let producto = listaProductos.filter(
-    (producto) => producto.id === id
-  );
-  console.log(producto[0]);
-  nameUser.value = producto[0].name
-  lastName.value = producto[0].lastName
-  idDocument.value = producto[0].idDocument
-  address.value = producto[0].address
-  phoneNumber.value = producto[0].phoneNumber
-  emailUser.value = producto[0].email
-}
 
 
-//Eliminar Producto
-async function eliminarProducto(id) {
-  await fetch("http://localhost:8080/api/users/delete/" + id, {
-    method: "DELETE",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((res) => {
-      console.log(res);
-      alert("Producto eliminado");
-      obtenerProductos();
-    })
-    .catch((err) => {
-      console.log(err);
-      alert("Lo sentimos no pudimos eliminar el producto");
-    });
-}
+
+
+
 
 
 //Reset Modal
@@ -169,6 +184,62 @@ window.onclick = function (event) {
     resetModal();
   }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const id = 0;
+
+async function prueba(id) {
+  //let productoGuardar
+  let productoModif = {
+    id:id,
+    name: nameUser.value,
+    lastName: lastName.value,
+    idDocument: idDocument.value,
+    address: address.value,
+    phoneNumber: phoneNumber.value,
+    email:emailUser.value
+  };
+  console.log(productoGuardar);
+
+  await fetch("http://localhost:8080/api/users/update/"+ id, {
+    method: "PUT",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(productoModif),
+  })
+    .then((producto) => {
+      console.log(producto);
+      alert("Usuario registrado");
+      modal.style.display = "none";
+      obtenerProductos();
+      resetModal();
+    })
+    .catch((e) => {
+      console.log(e);
+      alert("Error: " + e);
+    });
+}
+
+
+
+
+
+
+
 
 
 
